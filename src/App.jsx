4c268b544cfc08c4b9a8e61ad84a851ee1d4fd3a
Ali Aizad <aliaizad72@gmail.com>
2024-11-07 13:42:33 +0800

    Add new fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline font-serif">
             Hello world!
         </h1>
     )
