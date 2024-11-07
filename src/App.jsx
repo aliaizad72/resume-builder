5802c29ec2c12336c7aa4f9dcdb36da5a226ea21
Ali Aizad <aliaizad72@gmail.com>
2024-11-07 18:03:57 +0800

    Move state to parent object

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,55 @@ import './App.css'
 import Header from "./components/Header.jsx";
 import Description from "./components/Description.jsx";
 import Form from "./components/Form.jsx"
+import {useState} from "react";
 
 export default function App() {
+    const defaultResume = {
+        name: "",
+        email: "",
+        phone: "",
+        education: {
+            school: "",
+            title: "",
+            end: ""
+        },
+        experience: {
+            company: "",
+            position: "",
+            start: "",
+            end: ""
+        }
+    }
+
+    const [resume, setResume] = useState(defaultResume)
+
+    const createResumeObj = (e) => {
+        e.preventDefault()
+        const newResume = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            education: {
+                school: document.getElementById("school").value,
+                title: document.getElementById("studytitle").value,
+                end: document.getElementById("studyend").value
+            }
+            ,
+            experience: {
+                company: document.getElementById("company").value,
+                position: document.getElementById("position").value,
+                start: document.getElementById("workstart").value,
+                end: document.getElementById("workend").value
+            }
+        }
+        setResume(newResume)
+    }
+
     return (
         <>
             <Header />
             <Description />
-            <Form />
+            <Form resume={resume} submitFn={createResumeObj} />
         </>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,10 @@
 import GeneralInfo from "./form/Generalnfo.jsx";
 import Education from "./form/Education.jsx";
 import WorkExperience from "./form/WorkExperience.jsx";
-import {useState} from "react";
 
-export default function Form() {
-    const defaultResume = {
-        name: "",
-        email: "",
-        phone: "",
-        education: {
-            school: "",
-            title: "",
-            end: ""
-        },
-        experience: {
-            company: "",
-            position: "",
-            start: "",
-            end: ""
-        }
-    }
-
-    const [resume, setResume] = useState(defaultResume)
-
-    const createResumeObj = (e) => {
-        e.preventDefault()
-        const newResume = {
-                name: document.getElementById("name").value,
-                email: document.getElementById("email").value,
-                phone: document.getElementById("phone").value,
-                education: {
-                        school: document.getElementById("school").value,
-                        title: document.getElementById("studytitle").value,
-                        end: document.getElementById("studyend").value
-                }
-                ,
-                experience: {
-                        company: document.getElementById("company").value,
-                        position: document.getElementById("position").value,
-                        start: document.getElementById("workstart").value,
-                        end: document.getElementById("workend").value
-                }
-        }
-        setResume(newResume)
-    }
-
-
+export default function Form({ resume, submitFn }) {
     return (
-        <form onSubmit={createResumeObj} className="md:w-[768px] md:mx-auto mb-4 flex flex-col gap-5">
+        <form onSubmit={submitFn} className="md:w-[768px] md:mx-auto mb-4 flex flex-col gap-5">
             <GeneralInfo name={resume.name} email={resume.email} phone={resume.phone}/>
             <Education school={resume.education.school} title={resume.education.title} end={resume.education.end}/>
             <WorkExperience company={resume.experience.company} position={resume.experience.position} start={resume.experience.start} end={resume.experience.end} />
