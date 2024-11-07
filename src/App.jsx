6e551d1b2002ea614ef794607b40d04ca7ea9a8d
Ali Aizad <aliaizad72@gmail.com>
2024-11-07 13:54:20 +0800

    Complete description component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 import Header from "./components/Header.jsx";
+import Description from "./components/Description.jsx";
 
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <Description />
+        </>
     )
 }
---
+++ src/components/Description.jsx
@@ -0,0 +1,7 @@
+export default function Description() {
+    return (
+        <p className="p-2 text-center font-serif text-sm">
+            Key in your details and press submit to generate your resume!
+        </p>
+    )
+}
